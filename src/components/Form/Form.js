7908b7f9d20806af3fc15ad958e5d24d70429ec5
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-12-20 00:18:56 +0300

    Add react-hook-form and yup library

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,55 +1,119 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import styles from "./Form.module.css";
 
 const Form = () => {
-  const formElements = [
-    { name: "Ad", type: "text" },
-    { name: "Soyad", type: "text" },
-    { name: "TC", type: "text" },
-    { name: "Başvuru Nedeni", type: "textarea" },
-    { name: "Adres Bilgisi", type: "textarea" },
-    { name: "Fotoğraf Ekle", type: "file" },
-  ];
+  const schema = Yup.object({
+    name: Yup.string().required("Ad zorunlu"),
+    surname: Yup.string().required("Soyad zorunlu"),
+    idNumber: Yup.string().required("TC zorunlu"),
+    applicationReason: Yup.string().required("Başvuru Nedeni zorunlu"),
+    address: Yup.string().required("Adres Bilgisi zorunlu"),
+    photograph: Yup.mixed().test(
+      "required",
+      "Please select a file",
+      (files) => files?.length > 0
+    ),
+  }).required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.formElements}>
-        {formElements.map(({ name, type }, index) => (
-          <div
-            className={`${styles.formElement} ${
-              type === "textarea" ? styles.largeHeight : ""
-            }`}
-            key={index}
-          >
-            <label htmlFor={name}>{name}</label>
-            {type === "file" ? (
-              <div className={styles.fileUpload}>
-                <input
-                  className={styles.formInput}
-                  type="file"
-                  id={name}
-                  name={name.toLowerCase()}
-                />
-              </div>
-            ) : type === "textarea" ? (
-              <textarea
-                className={`${styles.formInput} ${styles.textarea}`}
-                id={name}
-                name={name.toLowerCase()}
-              />
-            ) : (
-              <input
-                className={styles.formInput}
-                type="text"
-                id={name}
-                name={name.toLowerCase()}
-              />
-            )}
-          </div>
-        ))}
-        <button className={styles.formSubmitButton} type="submit">
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.formElements}>
+        <div className={styles.formElement}>
+          <label htmlFor="name">Ad</label>
+          <input
+            {...register("name")}
+            type="text"
+            id="name"
+            className={styles.formInput}
+          />
+          {errors.name && (
+            <p className={styles.errorMessage}>{errors.name.message}</p>
+          )}
+        </div>
+
+        <div className={styles.formElement}>
+          <label htmlFor="surname">Soyad</label>
+          <input
+            {...register("surname")}
+            type="text"
+            id="surname"
+            className={styles.formInput}
+          />
+          {errors.surname && (
+            <p className={styles.errorMessage}>{errors.surname.message}</p>
+          )}
+        </div>
+
+        <div className={styles.formElement}>
+          <label htmlFor="idNumber">TC</label>
+          <input
+            {...register("idNumber")}
+            type="text"
+            id="idNumber"
+            className={styles.formInput}
+          />
+          {errors.idNumber && (
+            <p className={styles.errorMessage}>{errors.idNumber.message}</p>
+          )}
+        </div>
+
+        <div className={`${styles.formElement} ${styles.largeHeight}`}>
+          <label htmlFor="applicationReason">Başvuru Nedeni</label>
+          <textarea
+            {...register("applicationReason")}
+            id="applicationReason"
+            className={styles.formInput}
+          />
+          {errors.applicationReason && (
+            <p className={styles.errorMessage}>
+              {errors.applicationReason.message}
+            </p>
+          )}
+        </div>
+
+        <div className={`${styles.formElement} ${styles.largeHeight}`}>
+          <label htmlFor="address">Adres Bilgisi</label>
+          <textarea
+            {...register("address")}
+            id="address"
+            className={styles.formInput}
+          />
+          {errors.address && (
+            <p className={styles.errorMessage}>{errors.address.message}</p>
+          )}
+        </div>
+
+        <div className={styles.formElement}>
+          <label htmlFor="photograph">Fotoğraf Ekle</label>
+          <input
+            {...register("photograph")}
+            type="file"
+            id="photograph"
+            accept="image/jpeg, image/png, image/gif"
+          />
+          {errors.photograph && (
+            <p className={styles.errorMessage}>{errors.photograph.message}</p>
+          )}
+        </div>
+
+        <button type="submit" className={styles.formSubmitButton}>
           Gönder
         </button>
-      </div>
+      </form>
     </div>
   );
 };
